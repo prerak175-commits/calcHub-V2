1c5b3f0bc2c05765e327f88686ddc9c50857cf87
prerak175-commits <prerak175@gmail.com>
2026-06-07 23:14:24 +0530

    Update analytics.tsx

--- components/analytics/analytics.tsx
+++ components/analytics/analytics.tsx
@@ -1,23 +1,23 @@
+'use client';
+
+import Script from 'next/script';
+
 export function AnalyticsScripts() {
   return (
     <>
-      {/* Google Analytics 4 - Insert your GA4 Measurement ID below */}
-      {/* Example: <Script src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX" strategy="afterInteractive" /> */}
-      {/* <Script id="ga4-init" strategy="afterInteractive">
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-XXXXXXXXXX');`}
-      </Script> */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-47HD2XY1Y4"
+        strategy="afterInteractive"
+      />
 
-      {/* Microsoft Clarity - Insert your Clarity Project ID below */}
-      {/* <Script id="clarity-init" strategy="afterInteractive">
-        {`(function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "YOUR_CLARITY_PROJECT_ID");`}
-      </Script> */}
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-47HD2XY1Y4');
+        `}
+      </Script>
     </>
   );
 }
